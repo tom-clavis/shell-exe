3430e5e9a122f6f260178a19ca86761703616bfb
tom-clavis <tom.clavis@laplateforme.io>
2023-10-03 17:49:01 +0200

    Les scripts de gandalf

---
+++ Job05/hello_bye.sh
@@ -0,0 +1,7 @@
+if   [ $1 = hello ]
+then echo "Bonjour,je suis un script !"
+fi
+
+if [ $1 = bye ] 
+then echo  "Au revoir et bonne journée !"
+fi
---
+++ Job07/myloop.sh
@@ -0,0 +1,12 @@
+i=1
+
+while [ $i -le 10 ]
+
+do
+echo "je suis un script qui arrive à faire une boucle $i"
+
+((i++))
+
+done
+
+ 
